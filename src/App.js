c5aabf9b9c4a5d56870ae90a6b09ff3f5de358a5
stanvlv <114359328+stanvlv@users.noreply.github.com>
2023-01-03 18:53:24 +0100

    api component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import { getRecipes } from './components/Api';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  // on loading of the page we fetch the data and 
+  // save it in the state of recipes line 10-17
+
+  const [recipes, setRecipes] = useState([])
+
+  const promise = getRecipes()
+
+  useEffect(() => {
+    promise.then(recipe => setRecipes(recipe))
+    .catch(e => alert(e))
+  }, [])
+  // to see it - console.log(recipes)
+
   return (
     <div className="App">
       <h1>CookBook</h1>
