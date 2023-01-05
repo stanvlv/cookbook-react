4daf176c7a524274f5397038a44545a4e363099d
Jagholin <pavel.belskiy2@gmail.com>
2023-01-05 13:46:14 +0100

    make netlify bot happy

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,20 +4,23 @@ import axios from 'axios'
 export default function Image ({id}) {
 
     const [image, setImage] = useState('')
-
+    const [altText, setAltText] = useState('')
   
     useEffect(() => {
 
    
         axios
         .get(`https://cdn.contentful.com/spaces/mtjp1adh4yfh/environments/master/assets/${id}?access_token=3o4XsXdz9bndgQIjzV8QzAeu2c2qK18qaw9eT8sCvYA`)
-        .then(response => setImage(response.data.fields.file.url))
+        .then(response => {
+            setImage(response.data.fields.file.url)
+            setAltText(response.data.fields.description)
+        })
         .catch(error => console.log(error))
-    }, [])
+    }, [id])
   
 
     
     return <>
-        <img src={image} />
+        <img src={image} alt={altText}/>
         </>
 }
