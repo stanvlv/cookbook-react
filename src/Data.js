0a1b844e23cafa86aa94f4c6575ca192238c408b
stanvlv <114359328+stanvlv@users.noreply.github.com>
2023-01-05 16:05:28 +0100

    image.js deleted

--- src/Data.js
+++ src/Data.js
@@ -2,8 +2,8 @@ import { createClient } from 'contentful'
 
 // experimented with hiding in .env file 
 const data = createClient({
-      space: process.env.REACT_APP_NOTASECRET_SPACE_ID,
-      accessToken: process.env.REACT_APP_NOTASECRET_ACCESS_TOKEN
+      space: 'mtjp1adh4yfh', //process.env.REACT_APP_NOTASECRET_SPACE_ID,
+      accessToken: '3o4XsXdz9bndgQIjzV8QzAeu2c2qK18qaw9eT8sCvYA' //process.env.REACT_APP_NOTASECRET_ACCESS_TOKEN
 })
 
 export default data
