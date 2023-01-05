ab94b9ac45d09261d1158526ed8880bfde8e5973
Jagholin <pavel.belskiy2@gmail.com>
2023-01-05 17:41:58 +0100

    added data links to Recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,20 +4,30 @@ import {
   Col,
   Card
 } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import RichText from './RichText';
 
-export default function Recipe() {
+export default function Recipe({ recipes }) {
+  const { recipe_id } = useParams();
+
+  console.log(recipe_id);
+
+  const recipe = recipes.find((val) => val.sys.id === recipe_id);
+  console.log("recipe found: ", recipe)
+
   return (
     <div>
       <Row>
-      <Col>
+        <Col>
             <Card>
-              <Card.Img variant="top" src="holder.js/250px250" />
+              <Card.Img variant="top" src={recipe.fields.picture.fields.file.url} />
               <Card.Body>
-                <Card.Title>Recipe Title</Card.Title>
+                <Card.Title>{recipe.fields.title}</Card.Title>
                 <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  <h6>Ingredients</h6>
+                  <RichText content={recipe.fields.ingredients}/>
+                  <h6>Creation steps</h6>
+                  <RichText content={recipe.fields.creationSteps}/>
                 </Card.Text>
               </Card.Body>
             </Card>
