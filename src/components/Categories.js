9daafe6fd5dcd8e7ee88afd3f1999015dab85057
Jagholin <pavel.belskiy2@gmail.com>
2023-01-05 17:25:18 +0100

    added routing to Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Card, Col, Row, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { NavLink } from "react-router-dom";
 
 export default function Categories() {
+  const categories = ["Main", "Dessert", "Salad"];
+
   return (
     <div>
       <Row xs={1} md={3} className="g-4">
-        {Array.from({ length: 3 }).map((_, idx) => (
+        {categories.map((cat, idx) => (
           <Col>
             <Card className="text-center">
               <Card.Img variant="top" src="https://picsum.photos/60/20" />
               <Card.Body>
-                <Card.Title>Category Title</Card.Title>
-                <Button variant="dark">
-                  More
-                </Button>
+                <Card.Title>{cat}</Card.Title>
+                <NavLink to={`/category/${cat}`}>
+                  <Button variant="dark">
+                    More
+                  </Button>
+                </NavLink>
               </Card.Body>
             </Card>
           </Col>
