66d73e2c78c859f14de3f4991dbde6588fb32017
stanvlv <114359328+stanvlv@users.noreply.github.com>
2023-01-13 17:12:50 +0100

    scroll to top on pathchange

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
